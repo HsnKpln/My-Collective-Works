7b27a342e05f8473b9a022c0ed7073249a78d27b
HsnKpln <h.kpln0681@gmail.com>
2021-10-24 18:36:07 +0300

    Add project files.

---
+++ BOM Oyunu/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BOM_Oyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir BOM sayısı belirleyin..");
+            int bomSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Belirlenen BOM sayısı: {0}", bomSayisi);
+            for (int i = 1; i < 101; i++)
+            {
+                if (i%2 == 1)
+                {
+                    if (i%bomSayisi == 0)
+                    {
+                        Console.WriteLine("BOM");
+                    }
+                    else
+                    {
+                         Console.WriteLine("Bilgisayarın girdiği sayı: {0}",i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("sıradaki sayıyı giriniz..");
+                    int girilenSayi = Convert.ToInt32(Console.ReadLine());
+                    if (i%bomSayisi == 0)
+                    {
+                        string text = Console.ReadLine().ToUpper();
+                        if (text != "BOM")
+                        {
+                            Console.WriteLine("Kaybettiniz..");
+                            break;
+                        }
+                       
+                       
+                    }
+                    else
+                    {
+                        if (girilenSayi !=i)
+                        {
+                            Console.WriteLine("kaybettiniz..");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ InputIslemleri/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace InputIslemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Adınızı giriniz: ");
+            //string ad = Console.ReadLine();
+            //Console.WriteLine();
+
+            /*
+             * Kullanıcı girişleri
+             * Veritabanı istekleri (CRUD)
+             * Dosya işlemleri
+             * Sunucu istekleri
+             * Tür dönüşümleri
+             *
+             * Try - Catch | Error Handling
+             */
+
+            Console.Write("Lütfen 1 ile 100 arasında bir sayı giriniz: ");
+            int girilenSayi = 0;
+            try
+            {
+                girilenSayi = int.Parse(Console.ReadLine());
+                if (girilenSayi < 1 || girilenSayi > 100)
+                {
+                    throw new Exception("Girilen sayı 1-100 arasında değil.");
+                }
+
+                Console.WriteLine(girilenSayi * girilenSayi);
+            }
+            catch (OverflowException ex1)
+            {
+                Console.WriteLine("Girilen sayı çok büyük ya da çok küçük");
+            }
+            catch (FormatException ex2)
+            {
+                Console.WriteLine("Lütfen bir sayı giriniz");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally çalıştı");
+            }
+
+            Console.WriteLine("1-6 arası bir sayı giriniz");
+            int sayi2 = 0;
+            string girilen = Console.ReadLine();
+            if (int.TryParse(girilen, out sayi2))
+            {
+                
+                Console.WriteLine(sayi2 * sayi2 * sayi2);
+            }
+            else
+            {
+                Console.WriteLine("Bir sayı girmeyi beceremedin");
+            }
+        }
+    }
+}
